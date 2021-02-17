6686def45755658b8e2248ac628f9f020a88c95f
tokitu <852408680@qq.com>
2021-02-17 10:23:30 +0900

    Add files via upload

---
+++ WriteNumber/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.Numberwritting;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class AboutActivity extends Activity {   //AboutActivity类头部
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {   // onCreate()方法头部
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }   // onCreate()方法尾部
+    public void OnBack(View v){  		//实现关闭关于界面返回上一界面
+        AboutActivity.this.finish();   	//关闭关于界面
+    }
+
+}   //AboutActivity类尾部
+
